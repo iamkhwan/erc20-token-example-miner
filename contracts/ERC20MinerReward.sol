aa52f664dc1a11033cc67b813985cd4f7ee430ad
iamkwan <puttarak555@gmail.com>
2022-01-09 12:28:43 +0700

    Init project

---
+++ contracts/ERC20MinerReward.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20MinerReward is ERC20 {
+
+    event LogNewAlert(string description, address indexed _from, uint256 _n);
+
+    constructor() ERC20("MinerReward", "MRW") {}
+
+    function _reward() public {
+        _mint(block.coinbase, 20);
+        emit LogNewAlert('_rewarded', block.coinbase, block.number);
+    }
+}
+
